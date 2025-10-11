33680bcd458b6f798d70beb10d42a17dedca43a7
ksian45 <ksianp@miumg.edu.gt>
2025-10-11 01:14:23 -0600

    se agregaron clases

---
+++ proyectofinal/src/java/modelo/Marcas.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author UMG
+ */
+public class Marcas {
+    private int id_marca = 0;
+    private String marca;
+public Marcas(){}
+    
+    public Marcas(String marca) {
+        this.marca = marca;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getId_marca() {
+        return id_marca;
+    }
+
+    public void setId_marca(int id_marca) {
+        this.id_marca = id_marca;
+    }
+    
+    
+}
---
+++ proyectofinal/src/java/modelo/Productos.java
@@ -0,0 +1,92 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author UMG
+ */
+public class Productos {    
+    private int id_producto, id_marca, existencia;
+    private float precio_costo, precio_venta;
+    private String producto, descripcion, imagen;
+    public Productos(){}
+    public Productos(int id_producto, int id_marca, int existencia, float precio_costo, float precio_venta, String producto, String descripcion, String imagen) {
+        this.id_producto = id_producto;
+        this.id_marca = id_marca;
+        this.existencia = existencia;
+        this.precio_costo = precio_costo;
+        this.precio_venta = precio_venta;
+        this.producto = producto;
+        this.descripcion = descripcion;
+        this.imagen = imagen;
+    }
+
+    public int getId_producto() {
+        return id_producto;
+    }
+
+    public void setId_producto(int id_producto) {
+        this.id_producto = id_producto;
+    }
+
+    public int getId_marca() {
+        return id_marca;
+    }
+
+    public void setId_marca(int id_marca) {
+        this.id_marca = id_marca;
+    }
+
+    public int getExistencia() {
+        return existencia;
+    }
+
+    public void setExistencia(int existencia) {
+        this.existencia = existencia;
+    }
+
+    public float getPrecio_costo() {
+        return precio_costo;
+    }
+
+    public void setPrecio_costo(float precio_costo) {
+        this.precio_costo = precio_costo;
+    }
+
+    public float getPrecio_venta() {
+        return precio_venta;
+    }
+
+    public void setPrecio_venta(float precio_venta) {
+        this.precio_venta = precio_venta;
+    }
+
+    public String getProducto() {
+        return producto;
+    }
+
+    public void setProducto(String producto) {
+        this.producto = producto;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+    
+    
+}
